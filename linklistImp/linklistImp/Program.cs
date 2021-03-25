3ee2363400c3f0ad6df8399cc5b444a65701eb6b
amit <[amirana14325@gmail.com]>
2021-03-26 01:10:54 +0530

    [Amit Rana]Added::simple list 50,30,70

--- linklistImp/linklistImp/Program.cs
+++ linklistImp/linklistImp/Program.cs
@@ -7,7 +7,9 @@ namespace linklistImp
         static void Main(string[] args)
         {
             Console.WriteLine("WWelcome to my linklist demo program by Amit Rana");
-           
+            Node head = new Node(56);
+            head.next = new Node(30);
+            head.next.next = new Node(70);
         }
     }
 }
