8c6cf0a8d9ad2f035a1157e8a2b8c4846201a366
amit <[amirana14325@gmail.com]>
2021-03-26 01:17:30 +0530

    [Amit Rana]Added::creating linklist by appending 3 element

--- linklistImp/linklistImp/Program.cs
+++ linklistImp/linklistImp/Program.cs
@@ -8,9 +8,13 @@ namespace linklistImp
         {
             Console.WriteLine("WWelcome to my linklist demo program by Amit Rana");
             LinkedList list = new LinkedList();
-            list.Add(70);
-            list.Add(30);
-            list.Add(56);
+            /* list.Add(70);
+             list.Add(30);
+             list.Add(56);
+             list.Display();*/
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
             list.Display();
         }
     }
