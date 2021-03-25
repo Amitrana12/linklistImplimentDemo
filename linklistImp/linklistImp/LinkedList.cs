5babbd49ed4253db4c7a569800f14cd625e87a56
amit <[amirana14325@gmail.com]>
2021-03-26 01:08:38 +0530

    [Amit Rana]Added::Welcome massege

---
+++ linklistImp/linklistImp/LinkedList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linklistImp
+{
+    class LinkedList
+    {
+        internal Node head; 
+        public LinkedList()
+        {
+            this.head = null;
+        }
+       
+    }
+}
---
+++ linklistImp/linklistImp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace linklistImp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WWelcome to my linklist demo program by Amit Rana");
+           
+        }
+    }
+}
